3eb97c53343dd6925539676f4b25afd382642a5a
전제우 <junjewoo@jeonje-uui-MacBook-Pro.local>
2022-05-01 22:10:11 +0900

    struct, fuction update

--- moneybook.h
+++ moneybook.h
@@ -3,12 +3,12 @@ typedef struct{
 	char name[20]; // 이름
 	int price; // 가격
 	char kategorie[256]; // 카테고리
-	char content[256]; // 비고(필요시 추가)
+	char fix; // 고정/변동(y/n)
 } moneybook;
 
 int selectMenu();
 
-void listBook(moneybook *m, int count); // 전체 가계부 상태 출력 (sum)
+void listBook(moneybook *m, int count); // 전체 가계부 상태 출력 (sum출력 필요)
 void readBook(moneybook m); // 하나의 항목 출력
 int createBook(moneybook *m); // 항목 추가
 
@@ -19,8 +19,8 @@ void sumBook(moneybook *m); // 항목 합계 계산
 
 int selectDataNo(moneybook *m, int count); // 항목 번호 검색
 void searchBook(moneybook *m, int count); // 항목 이름 검색
-void searchPrice(moneybook *m, int count); // 가격 검색
-void searchKategorie(moneybook *m, int count); // 가계부 카테고리 검색 (sum)
+void searchKategorie(moneybook *m, int count); // 가계부 카테고리 검색 (sum출력 필요)
+void searchFix(moneybook *m, int count); // 고정/변동 검색
 
 void saveData(moneybook *m, int count); // File 저장
 int loadData(moneybook *m); // File에서 데이터 불러오기
